907d51748caef9153b9b20c9f6b1e7cb3eae77e1
AAMSK <73601536+AAMSK@users.noreply.github.com>
2022-01-26 14:35:50 +0600

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -22,7 +23,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
   final String title;
 
   @override
@@ -30,80 +30,257 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  set shape(BoxShape shape) {}
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.indigo[400],
-        child: Center(
-          child: Container(
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Container(
-                      color: Colors.amberAccent,
-                      height: MediaQuery.of(context).size.height / 2,
-                      width: MediaQuery.of(context).size.width / 2,
+      appBar: new AppBar(
+        title: new FlutterLogo(
+          textColor: Colors.green,
+          size: 25.0,
+        ),
+        elevation: 10.0,
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.arrow_back),
+          color: Colors.grey,
+        ),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.menu),
+          )
+        ],
+      ),
+      body: ListView(
+        shrinkWrap: true,
+        children: <Widget>[
+          Stack(
+            children: <Widget>[
+              Container(
+                alignment: const Alignment(0.0, -0.40),
+                height: 100.0,
+                color: Colors.white,
+                child: const Text(
+                  'First UI',
+                  style: TextStyle(fontFamily: 'Montserrat', fontSize: 20.0),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.fromLTRB(25.0, 90.0, 25.0, 0.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20.0),
+                  boxShadow: const [
+                    BoxShadow(
+                      blurRadius: 2.0,
+                      color: Colors.grey,
+                    )
+                  ],
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Stack(
+                      children: <Widget>[
+                        Container(
+                          padding:
+                              const EdgeInsets.fromLTRB(25.0, 25.0, 5.0, 5.0),
+                          child: const Text(
+                            'Your Friends',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontFamily: 'Quicksand',
+                              fontWeight: FontWeight.bold,
+                              fontSize: 14.0,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          padding:
+                              const EdgeInsets.fromLTRB(40.0, 40.0, 5.0, 5.0),
+                          child: const Text(
+                            '80',
+                            style: TextStyle(
+                              color: Colors.lightBlueAccent,
+                              fontFamily: 'Quicksand',
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30.0,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      width: 100.0,
                     ),
                     Container(
-                      color: Colors.blue,
-                      height: MediaQuery.of(context).size.height / 2,
-                      width: MediaQuery.of(context).size.width / 2,
+                      padding: const EdgeInsets.fromLTRB(5.0, 5.0, 5.0, 5.0),
+                      height: 50.0,
+                      width: 125.0,
+                      decoration: BoxDecoration(
+                        color: Colors.lightGreenAccent.withOpacity(0.8),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: const Center(
+                        child: Text(
+                          'See All',
+                          style: TextStyle(
+                            fontFamily: 'Quicksand',
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.green,
+                          ),
+                        ),
+                      ),
                     ),
                   ],
                 ),
-                Row(
-                  children: [
-                    Container(),
-                    Container(),
-                  ],
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 40.0,
+          ),
+          Container(
+            padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const <Widget>[
+                Text(
+                  'See Friends',
+                  style: TextStyle(
+                    color: Colors.lightGreen,
+                  ),
+                ),
+                Text(
+                  'You Requested',
+                  style: TextStyle(
+                    color: Colors.lightGreen,
+                  ),
                 ),
               ],
             ),
           ),
-        ),
+          const SizedBox(height: 10.0),
+          GridView.count(
+            crossAxisCount: 2,
+            primary: false,
+            crossAxisSpacing: 2.0,
+            mainAxisSpacing: 4.0,
+            shrinkWrap: true,
+            children: <Widget>[
+              _buildCard('Tom', 'Available', 1),
+              _buildCard('Tom', 'Away', 2),
+              _buildCard('Tom', 'Available', 3),
+              _buildCard('Tom', 'Away', 4),
+              _buildCard('Tom', 'Available', 5),
+              _buildCard('Tom', 'Away', 6),
+            ],
+          ),
+        ],
       ),
-      //
-      //
-      //
-      //
-      //
-      //
-      // Center(
-      //   child: Container(
-
-      //     color: Colors.black,
-      //     height: MediaQuery.of(context).size.height / 2,
-      //     width: MediaQuery.of(context).size.width / 2,
-      //     child: Column(
-      //       children: [
-      //         Container(
-      //             color: Colors.purpleAccent[400],
-      //             height: MediaQuery.of(context).size.height / 10,
-      //             width: MediaQuery.of(context).size.width),
-      //         Container(
-      //             color: Colors.yellow[900],
-      //             height: MediaQuery.of(context).size.height / 10,
-      //             width: MediaQuery.of(context).size.width),
-      //         Container(
-      //             color: Colors.redAccent[400],
-      //             height: MediaQuery.of(context).size.height / 10,
-      //             width: MediaQuery.of(context).size.width),
-      //         Container(
-      //             color: Colors.green[600],
-      //             height: MediaQuery.of(context).size.height / 10,
-      //             width: MediaQuery.of(context).size.width),
-      //         Container(
-      //             color: Colors.limeAccent[700],
-      //             height: MediaQuery.of(context).size.height / 10,
-      //             width: MediaQuery.of(context).size.width),
-      //       ], // children
-      //     ),
-      //   ),
-      // ),
     );
-  } // build
-} // class
+  }
+
+  Widget _buildCard(String name, String status, int cardIndex) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      elevation: 7.0,
+      child: Column(
+        children: <Widget>[
+          const SizedBox(height: 12.0),
+          Stack(
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.fromLTRB(60, 10, 60, 10),
+                height: 50.0,
+                width: 50.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30.0),
+                  color: Colors.green,
+                  image: const DecorationImage(
+                    image: NetworkImage(
+                        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_g_7YVzERozXI_mfnbSPkggiXqlljwtCQXw&usqp=CAU'),
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.fromLTRB(95, 9, 0, 0),
+                height: 15.0,
+                width: 15.0,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30.0),
+                    color: status == 'Away' ? Colors.amber : Colors.green,
+                    border: Border.all(
+                      color: Colors.white,
+                      style: BorderStyle.solid,
+                      width: 3.0,
+                    )),
+              ),
+              const SizedBox(height: 8.0),
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.fromLTRB(70.0, 100.0, 70.0, 20.0),
+                  child: Text(
+                    name,
+                    style: const TextStyle(
+                      fontFamily: 'Quicksand',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0,
+                      color: Colors.red,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 5.0),
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.fromLTRB(70.0, 140, 70.0, 20.0),
+                  child: Text(
+                    status,
+                    style: const TextStyle(
+                      fontFamily: 'Quicksand',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 13.0,
+                      color: Colors.yellow,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 150.0),
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.only(top: 170.0),
+                  padding: const EdgeInsets.fromLTRB(0, 0, 0, 5),
+                  width: 175.0,
+                  decoration: BoxDecoration(
+                    color: status == 'Away' ? Colors.grey : Colors.green,
+                    borderRadius: BorderRadius.circular(
+                      (10.0),
+                    ),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      'Request',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.white,
+                        fontFamily: 'Quicksand',
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+      margin: cardIndex.isEven
+          ? EdgeInsets.fromLTRB(10.0, 0.0, 25.0, 10.0)
+          : EdgeInsets.fromLTRB(25.0, 0.0, 5.0, 10.0),
+    );
+  }
+}
